fb8e6627784cba56867d7abc88c8dae3f0f0611a
PaulP1406 <ducduy0308@gmail.com>
2026-02-01 15:22:04 -0800

    new schema for practice

--- unify/schemaTypes/index.ts
+++ unify/schemaTypes/index.ts
@@ -2,10 +2,12 @@ import module from './module'
 import submodule from './submodule'
 import lesson from './lesson'
 import quiz from './quiz'
+import practice from './practice'
 
 export const schemaTypes = [
   module,
   submodule,
   lesson,
   quiz,
+  practice,
 ]
